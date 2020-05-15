fe10da434ed7ca85055b3ab30bdfe7b78206aac7
mariaPaula <maria.paula.mosquera@correounivalle.edu.co>
2020-05-14 23:49:39 -0500

    commit necesario para cambiarme de rama

--- srcback/settings.py
+++ srcback/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'srcback.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wattlab_produccion',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'wattlab.cjbpeuvptazq.us-east-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
