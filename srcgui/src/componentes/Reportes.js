05caee41e0eea35cf04ab5461d1dd99ccd3a60d2
mariaPaula <maria.paula.mosquera@correounivalle.edu.co>
2020-05-19 13:01:54 -0500

    Reportes ordenado modo responsive y agregué cositas

--- srcgui/src/componentes/Reportes.js
+++ srcgui/src/componentes/Reportes.js
@@ -12,56 +12,56 @@ function Reportes(props) {
                 descripcion={i18n.t('reports-panel.rpt_int-description')} />
             <div className="container" style={{ display: 'flex', maxWidth: 900, marginBottom: "20px" }}>
                 <div className="col" >
-                    <div className="row" style={{width: "900px", height:"200px", padding: "10px"}} >
-                        <div className="col" >
+                    <div className="row" >
+                        <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12" style={{marginBottom: "20px"}}>
                             <h3 align="center"> {i18n.t('reports-panel.rpt_clientes')}</h3>
                             <p></p>
-                            <h2 align="center">{props.reporte.clientes}</h2>
+                            <h2 align="center" style={{borderRadius: "15px", background: "#3298d1"}}>{props.reporte.clientes}</h2>
                         </div>
-                        <div className="col">
+                        <div className="col-lg-4 col-md-6  col-sm-12 col-xs-12" style={{marginBottom: "20px"}}>
                             <h3 align="center">{i18n.t('reports-panel.rpt_cntrt-activo')}</h3>
                             <p></p>
-                            <h2 align="center">{props.reporte.contrato}</h2>
+                            <h2 align="center" style={{borderRadius: "15px", background: "#32d156"}}>{props.reporte.contrato}</h2>
                         </div>
-                        <div className="col" >
+                        <div className="col-lg-4 col-md-12  col-sm-12 col-xs-12" style={{marginBottom: "20px"}}>
                             <h3 align="center">{i18n.t('reports-panel.rpt_cntd-sube')}</h3>
                             <p></p>
-                            <h2 align="center">{props.reporte.subE}</h2>
-                        </div>
-                    </div>
-
-                    <div className="row" style={{width: "900px", height:"200px", padding: "10px"}}>
-                        <div className="col">
-                            <h3 align="center">{i18n.t('reports-panel.rpt_cntd-trns')}</h3>
-                            <p></p>
-                            <h2 align="center">{props.reporte.trnsf}</h2>
-                        </div>
-                        <div className="col" >
-                            <h3 align="center">{i18n.t('reports-panel.rpt_conv-act-banco')}</h3>
-                            <p></p>
-                            <h2 align="center">{props.reporte.banco_Activo}</h2>
-                        </div>
-                        <div className="col" >
-                            <h3 align="center">{i18n.t('reports-panel.rpt_conv-inct-banco')}</h3>
-                            <p></p>
-                            <h2 align="center">{props.reporte.banco_Inctivo}</h2>
-                        </div>
-                    </div>
-                    <div className="row" style={{ width: "900px", height: "200px", padding: "10px" }}>
-                        <div className="col">
-                            <h3 align="center">{i18n.t('reports-panel.rpt_prm-enrg')}</h3>
-                            <p></p>
-                            <h2 align="center">{props.reporte.consu_ener}</h2>
-                        </div>
-                        <div className="col" >
-                            <h3 align="center">{i18n.t('reports-panel.rpt_ingr-dinero')}</h3>
-                            <p></p>
-                            <h2 align="center">{props.reporte.dinero}</h2>
+                            <h2 align="center" style={{borderRadius: "15px", background: "#d381ff"}}>{props.reporte.subE}</h2>
                         </div>
                     </div>
 
                     <div className="row">
-                        <div className="col">
+                        <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12" style={{marginBottom: "20px"}}>
+                            <h3 align="center">{i18n.t('reports-panel.rpt_cntd-trns')}</h3>
+                            <p></p>
+                            <h2 align="center" style={{borderRadius: "15px", background: "#EE3F69"}}>{props.reporte.trnsf}</h2>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-12 col-xs-12" style={{marginBottom: "20px"}}>
+                            <h3 align="center">{i18n.t('reports-panel.rpt_conv-act-banco')}</h3>
+                            <p></p>
+                            <h2 align="center" style={{borderRadius: "15px", background: "#eabf2e"}}>{props.reporte.banco_Activo}</h2>
+                        </div>
+                        <div className="col-lg-4 col-md-12 col-sm-12 col-xs-12" style={{marginBottom: "20px"}}>
+                            <h3 align="center">{i18n.t('reports-panel.rpt_conv-inct-banco')}</h3>
+                            <p></p>
+                            <h2 align="center" style={{borderRadius: "15px", background: "#00E09C"}}>{props.reporte.banco_Inctivo}</h2>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12" style={{marginBottom: "20px"}}>
+                            <h3 align="center">{i18n.t('reports-panel.rpt_prm-enrg')}</h3>
+                            <p></p>
+                            <h2 align="center" style={{borderRadius: "15px", background: "#ea2e2e"}}>{props.reporte.consu_ener}</h2>
+                        </div>
+                        <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12" style={{marginBottom: "20px"}}>
+                            <h3 align="center">{i18n.t('reports-panel.rpt_ingr-dinero')}</h3>
+                            <p></p>
+                            <h2 align="center" style={{borderRadius: "15px", background: "#d381ff"}}>{props.reporte.dinero}</h2>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col col-md-12 col-sm-12 col-xs-12" style={{marginBottom: "20px"}}>
                             <Chart
                                 width={400}
                                 height={'300px'}
@@ -72,7 +72,6 @@ function Reportes(props) {
                                     ['2019', 600, 400],
                                     ['2020', 1000, 600],
                                     ['2021', 0, 0],
-                                    ['2022', 0, 0],
                                 ]}
                                 options={{
                                     title: 'Reporte de ingresos',
