f7014222dff1a789a5d68fc6945402cf665b4ad0
juancardona97 <juan.david.castro@correounivalle.edu.co>
2020-05-20 19:13:49 -0500

    Cambios

--- srcgui/src/componentes/ConsultaFactura.js
+++ srcgui/src/componentes/ConsultaFactura.js
@@ -7,11 +7,8 @@ import Table from '../container/Table'
 import ModificarUse from './ModificarUse'
 import BackService from '../store/PeticionesBack';
 import BotonVisualizar from './BotonVisualizar';
-import Hola from './Hola'
-import FacturaPrue from './FacturaPrue';
-import PaymentForm from './PaymentForm';
+import Factura from './Factura';
 const solicitudBack = new BackService();
-
 
 class ConsultaFactura extends React.Component {
     state = {
@@ -32,7 +29,7 @@ class ConsultaFactura extends React.Component {
         return (
             <React.Fragment>
                 <div className="container pre-scrollable" style={{ marginTop: "10px", maxHeight: "350px", marginBottom: "20px" }}>
-                    <Table t1='Id' t2='Valor consumo' t3='Valor mora' t4='Valor reconexión' t5='Valor a pagar' t6='Fecha pago' t7='Fecha corte' t8='Visualizar' tabla='factura' datos={this.state.datos} />
+                    <Table t1='Id' t2='Valor consumo' t3='Valor mora' t4='Valor reconexión' t5='Valor a pagar' t6='Fecha pago' t7='Fecha corte' t8='Visualizar' tabla='factura' datos={this.state.datos}/>
                 </div>
             </React.Fragment>
         )
@@ -47,20 +44,8 @@ class ConsultaFactura extends React.Component {
 
     onKeyPressed = (e) => {
         if (e.keyCode === 8) {
-            // this.solicitud()
+           // this.solicitud()
         }
-    }
-
-    ver = () => {
-        this.setState({
-            bandera: true
-        })
-    }
-    mostrar = () => {
-        console.log(this.state.bandera)
-        if (this.state.bandera === true) {
-            return <Hola hola="Esta funcionando"/>
-        } else return null
     }
 
     buscador = (numero) => {
@@ -117,13 +102,11 @@ class ConsultaFactura extends React.Component {
                             {this.mostrarTable()}
                         </div>
                     </form>
-                    <div></div><BotonVisualizar verContrato={this.ver} /></div>
-                    <PaymentForm/>
-                
-                <div></div>
+                    <div><BotonVisualizar/></div>
+                    <div><Factura/></div>
                     
-                {this.mostrar()}
-            </Layout >
+                </div>
+            </Layout>
         );
     }
 }
--- srcgui/src/componentes/Factura.js
+++ srcgui/src/componentes/Factura.js
@@ -4,7 +4,7 @@ import PDF, { Text, AddPage, Line, Image, Table, Html } from 'jspdf-react'
 
 import { useTranslation } from 'react-i18next';
 
-import React, { Component } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 
 import ImagePubilidad from './Image'
 
@@ -18,95 +18,126 @@ const styleH1 = {
 const invisibleStyle = {
   display: 'none',
 };
-const i18n = useTranslation();
 
+function Factura(props) {
+    const i18n = useTranslation();
+    var titulo = props.h1;
+    var nameBtn = props.nameBtn;
+    //La referencia para poder enfocar
+    const myRef = useRef();
+    const myRef2 = useRef();
 
-class Factura extends Component {
+    //Para inactivar o activar tomar el id del cliente, el estado, y el profile vacio, 
+    //Para modificar crear un segundo estado con el id, nmro_idntfccn, prmr_nmbre, prmr_aplldo, email, tpo_idntfcn
 
-  //Para inactivar o activar tomar el id del cliente, el estado, y el profile vacio, 
-  //Para modificar crear un segundo estado con el id, nmro_idntfccn, prmr_nmbre, prmr_aplldo, email, tpo_idntfcn
+    //Los estados, los cuales almacenan los valores de los inputs
+    const [factura, setFactura] = useState({
+      id: 2,
+      cnsctvo_cnsmo: {
+          id: 4,
+          kwh: 110,
+          prdo_cnsmo: "202007",
+          obsrvcn: "Registro de consumo",
+          idntfccn_cntrto: {
+              id: 2,
+              estrt_scl: 1,
+              drccn: "Calle 72q #27o",
+              estado: true,
+              cliente: 2
+          }
+      },
+      cnsctvo_trfa: {
+          id: 1,
+          vlr_kwh: 9.501,
+          inco_vgca: "2020-05-15",
+          obsrvcn: "Tarifa kwh año 2020",
+          estdo: true
+      },
+      vlr_cnsmo: 171.018,
+      vlr_intrss_mra: 0.0,
+      vlr_rcnxn: 0.0,
+      vlr_ttl: 171.018,
+      fcha_lmte_pgo: "2020-05-22",
+      cntdd_fctrs_pndts: 0,
+      fcha_crte_srvco: "2020-05-30",
+      obsrvcn: "Ningún problema",
+      estado: false
+    })
 
-  //Los estados, los cuales almacenan los valores de los inputs
-  state = {
-    id: 2,
-    cnsctvo_cnsmo: {
-      id: 4,
-      kwh: 110,
-      prdo_cnsmo: "202007",
-      obsrvcn: "Registro de consumo",
-      idntfccn_cntrto: {
-        id: 2,
-        estrt_scl: 1,
-        drccn: "Calle 72q #27o",
-        estado: true,
-        cliente: 2
-      }
-    },
-    
-    cnsctvo_trfa: {
-      id: 1,
-      vlr_kwh: 9.501,
-      inco_vgca: "2020-05-15",
-      obsrvcn: "Tarifa kwh año 2020",
-      estdo: true
-    },
-    vlr_cnsmo: 171.018,
-    vlr_intrss_mra: 0.0,
-    vlr_rcnxn: 0.0,
-    vlr_ttl: 171.018,
-    fcha_lmte_pgo: "2020-05-22",
-    cntdd_fctrs_pndts: 0,
-    fcha_crte_srvco: "2020-05-30",
-    obsrvcn: "Ningún problema",
-    estado: false
-  }
-
-  
-
-  render() {
-    var titulo = this.props.h1;
-    var nameBtn = this.props.nameBtn;
+    const actualizar = () => {
+      setFactura({
+          ...factura,
+          id: props.idRow,
+          cnsctvo_cnsmo: {
+            id: props.idConsumo,
+            kwh: props.kwg,
+            prdo_cnsmo: props.periodoConsumo,
+            obsrvcn: props.observacionConsumo,
+            idntfccn_cntrto: {
+                id: props.idContrato,
+                estrt_scl: props.estrato,
+                drccn: props.direccion,
+                estado: props.estadoContrato,
+                cliente: props.idCliente
+            }
+          },
+          cnsctvo_trfa: {
+            id: props.idTarifa,
+            vlr_kwh: props.valorKwh,
+            inco_vgca: props.inicioVigencia,
+            obsrvcn: props.observacionTarifa,
+            estdo: props.estadoTarifa
+          },
+          vlr_cnsmo: props.valorConsumo,
+          vlr_intrss_mra: props.interesMora,
+          vlr_rcnxn: props.valorReconexion,
+          vlr_ttl: props.valorTotal,
+          fcha_lmte_pgo: props.fechaLimitePago,
+          cntdd_fctrs_pndts: props.cantidadFacturasPendientes,
+          fcha_crte_srvco: props.FechaCorteServicio,
+          obsrvcn: props.observacionFactura,
+          estado: props.estadoFactura
+      })
+    }
 
     const properties = { header: 'Acme' }
     const head = [["Item", "Nombre", "Valor"]]
     const body = [
-      [1, "kWh consumidos", this.state.cnsctvo_cnsmo.kwh],
-      [2, "Periodo consumo", this.state.cnsctvo_cnsmo.prdo_cnsmo],
-      [3, "Valor KwH", "$" + this.state.cnsctvo_trfa.vlr_kwh],
-      [4, "Vigencia tarifa", this.state.cnsctvo_trfa.obsrvcn],
-      [5, "Valor consumo", "$" + this.state.vlr_cnsmo],
-      [6, "Valor mora", "$" + this.state.vlr_intrss_mra],
-      [7, "Valor reconexión", "$" + this.state.vlr_rcnxn],
-      [8, "Valor total a pagar", "$" + this.state.vlr_ttl],
+        [1, "kWh consumidos", factura.cnsctvo_cnsmo.kwh],
+        [2, "Periodo consumo", factura.cnsctvo_cnsmo.prdo_cnsmo],
+        [3, "Valor KwH", "$"+factura.cnsctvo_trfa.vlr_kwh],
+        [4, "Vigencia tarifa", factura.cnsctvo_trfa.obsrvcn],
+        [5, "Valor consumo", "$"+factura.vlr_cnsmo],
+        [6, "Valor mora", "$"+factura.vlr_intrss_mra],
+        [7, "Valor reconexión", "$"+factura.vlr_rcnxn],
+        [8, "Valor total a pagar", "$"+factura.vlr_ttl],
     ]
     const head1 = [["Fecha límite de pago", "Fecha de corte de servicio"]]
     const body1 = [
-      [this.state.fcha_lmte_pgo, this.state.fcha_crte_srvco],
+      [factura.fcha_lmte_pgo, factura.fcha_crte_srvco],
     ]
     const head2 = [["Observaciones"]]
-    const body2 = [[this.state.obsrvcn]]
+    const body2 = [[factura.obsrvcn]]
 
     const head3 = [["# Contrato", "ID Cliente", "Estrato", "Dirección residencia"]]
-    const body3 = [[this.state.cnsctvo_cnsmo.idntfccn_cntrto.id, this.state.cnsctvo_cnsmo.idntfccn_cntrto.cliente, this.state.cnsctvo_cnsmo.idntfccn_cntrto.estrt_scl, this.state.cnsctvo_cnsmo.idntfccn_cntrto.drccn]]
-    const body4 = [["TOTAL A PAGAR", "$" + this.state.vlr_ttl]]
-
+    const body3 = [[factura.cnsctvo_cnsmo.idntfccn_cntrto.id, factura.cnsctvo_cnsmo.idntfccn_cntrto.cliente, factura.cnsctvo_cnsmo.idntfccn_cntrto.estrt_scl, factura.cnsctvo_cnsmo.idntfccn_cntrto.drccn]]
+    const body4 = [["TOTAL A PAGAR", "$"+factura.vlr_ttl]]
     return (
       <React.Fragment>
         <PDF properties={properties} preview={true}>
-          <Text x={23} y={30} size={40} style>Factura de servicios públicos</Text>
-          <Table head={head1} body={body1} />
-          <Table head={head3} body={body3} />
-          <Table head={head} body={body} />
-          <Table head={head2} body={body2} />
-          <Table body={body4} />
-          <Image src={ImagePubilidad} x={15} y={230} width={180} height={60}></Image>
-          <Html sourceById='page' />
+        <Text x={23} y={30} size={40} style>Factura de servicios públicos</Text>
+            <Table head={head1} body={body1}/>
+            <Table head={head3} body={body3}/>
+            <Table head={head} body={body}/>
+            <Table head={head2} body={body2}/>
+            <Table body={body4}/>
+            <Image src={ImagePubilidad}  x={15} y={230} width={180} height={60}></Image>
+            <Html sourceById='page'/>
         </PDF>
         <div id="page" style={invisibleStyle}></div>
       </React.Fragment>
     )
   }
-}
 
 
 export default Factura;
